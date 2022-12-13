b71089d156f31a063214d798fc4d2c7deadc8c72
Vincent <vinelb050125@student.jenseneducation.se>
2022-12-13 13:52:52 +0100

    Add project files.

---
+++ Klasser upg.3/Fågel.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Klasser_upg._3
+{
+    public class Fågel
+    {
+        private int Vikt = 0;
+        private int Längd = 0;
+        private string Namn = "okänd";
+
+        public int getVikt()
+        {
+            return this.Vikt;
+        }
+        public int getLängd()
+        {
+            return this.Längd;
+        }
+        public string getNamn()
+        {
+            return this.Namn;
+        }
+        public void setLängd(int längd)
+        {
+            this.Längd = längd;
+        }
+        public void setVikt(int vikt)
+        {
+            this.Vikt = vikt;
+        }
+        public void setNamn(string namn)
+        {
+            this.Namn = namn;
+        }
+    }
+}
---
+++ Klasser upg.3/Program.cs
@@ -0,0 +1,27 @@
+﻿
+
+
+using Klasser_upg._3;
+
+Console.WriteLine("Ser du fågeln?");
+Fågel fågel1 = new Fågel();
+
+fågel1.setVikt(530);
+fågel1.setLängd(48);
+fågel1.setNamn("Kroka");
+
+Console.WriteLine(fågel1.getNamn());
+Console.WriteLine(fågel1.getVikt());
+Console.WriteLine(fågel1.getLängd());
+
+
+Fågel fågel2 = new Fågel();
+
+fågel2.setVikt(5800);
+fågel2.setLängd(85);
+fågel2.setNamn("Örn");
+
+Console.WriteLine(fågel2.getNamn());
+Console.WriteLine(fågel2.getVikt());
+Console.WriteLine(fågel2.getLängd());
+
